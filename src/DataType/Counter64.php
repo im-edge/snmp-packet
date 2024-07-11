de6e95a8ab5590e1ccca7d1386b7b6f6ba60036f
Thomas Gelf <thomas@gelf.net>
2024-07-11 12:06:51 +0200

    Counter64: improve large value handling. Still...
    ...not happy

--- src/DataType/Counter64.php
+++ src/DataType/Counter64.php
@@ -7,6 +7,7 @@ use Sop\ASN1\Element;
 use Sop\ASN1\Type\Primitive\Integer;
 use Sop\ASN1\Type\Tagged\ApplicationType;
 use Sop\ASN1\Type\UnspecifiedType;
+use Sop\ASN1\Util\BigInt;
 
 class Counter64 extends DataType
 {
@@ -15,14 +16,23 @@ class Counter64 extends DataType
 
     final protected function __construct(ApplicationType $app)
     {
-        $tag = $this->getTag();
-        $value = $app->asImplicit(Element::TYPE_INTEGER, $tag)->asInteger()->intNumber();
-        if ($value < 0 || $value > 18446744073709551615) {
-            throw new InvalidArgumentException(
-                '%s is not a valid Counter64'
-            );
+        // TODO: double-check and test this - I'm not very happy with this solution
+        $value = $app->asImplicit(Element::TYPE_INTEGER, static::TAG)->asInteger()->number();
+        $bigInt = new BigInt($value);
+        $bigInt = BigInt::fromUnsignedOctets($bigInt->signedOctets());
+        // Hint: 2^64-1 gets converted to float with PHP. For performance reason, doing a rough check first
+        $obj = $bigInt->gmpObj();
+        if (
+            (gmp_sign($obj) === -1)
+            || (gmp_cmp($obj, '18446744073709551615') === 1)
+        ) {
+            throw new InvalidArgumentException(sprintf(
+                '%s is not a valid Counter64',
+                var_export($value, true)
+            ));
         }
-        parent::__construct($value);
+
+        parent::__construct($obj);
     }
 
     public static function fromASN1(UnspecifiedType $element): DataType|static
@@ -38,8 +48,8 @@ class Counter64 extends DataType
     public function jsonSerialize(): array
     {
         return [
-            'type'  => self::TYPE_TO_NAME_MAP[$this->getTag()],
-            'value' => $this->rawValue,
+            'type'  => self::TYPE_TO_NAME_MAP[static::TAG],
+            'value' => gmp_strval($this->rawValue, 10),
         ];
     }
 }
