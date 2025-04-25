ce9791d0a0c65892ca62ab8ba323e691d1c563ed
Thomas Gelf <thomas@gelf.net>
2025-04-25 19:23:41 +0200

    VarBind: use new Values, add List

---
+++ src/Message/VarBindList.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace IMEdge\Snmp\Message;
+
+use FreeDSx\Asn1\Type\SequenceType;
+use IMEdge\Snmp\VarBindValue\VarBindValue;
+use OutOfRangeException;
+use ValueError;
+
+class VarBindList
+{
+    protected function __construct(
+        /** @var VarBind[] */
+        protected array $varBinds = []
+    ) {
+    }
+
+    public function index(int $index): VarBind
+    {
+        return $this->varBinds[$index - 1] ?? throw new OutOfRangeException("There is no VarBind at idx=$index");
+    }
+
+    public static function fromAsn1(SequenceType $sequence): VarBindList
+    {
+        $list = [];
+        foreach ($sequence->getChildren() as $idx => $varBind) {
+            if (! $varBind instanceof SequenceType) {
+                throw new ValueError(sprintf('VarBind at idx=%d is not a Sequence', $idx + 1));
+            }
+            try {
+                $list[$idx] = VarBind::fromAsn1($varBind);
+            } catch (ValueError $e) {
+                throw new ValueError(sprintf(
+                    "Can't decode Variable Binding %d: %s",
+                    $idx + 1,
+                    $e->getMessage()
+                ), 0, $e);
+            }
+        }
+
+        return new VarBindList($list);
+    }
+
+    public function toAsn1(): SequenceType
+    {
+        return new SequenceType(...array_map(fn ($varBind) => $varBind->toASN1(), $this->varBinds));
+    }
+}
